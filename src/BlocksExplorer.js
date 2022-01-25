54550f6b3fbb2bc372cf45a7504b89fd084d5d23
Patryk Piecuch <patrykpiecuch@protonmail.com>
2022-01-25 02:24:31 +0100

    Add animated display of blocks on the page

--- src/BlocksExplorer.js
+++ src/BlocksExplorer.js
@@ -7,6 +7,9 @@ const BlocksExplorer = () => {
 
   useEffect(() => {
     const ws = new WebSocket("wss://ws.blockchain.info/inv");
+    const blocksArray = [];
+    let setBlocksTimeout = null;
+    let setPingInterval = null;
     const apiCallBlocks = {
       op: "blocks_sub",
     };
@@ -14,36 +17,53 @@ const BlocksExplorer = () => {
       op: "ping",
     };
 
-    ws.onopen = (event) => {
+    setPingInterval = setInterval(
+      () => ws.send(JSON.stringify(apiCallPing)),
+      60000
+    );
+
+    ws.onopen = () => {
       ws.send(JSON.stringify(apiCallBlocks));
     };
-    setInterval(() => ws.send(JSON.stringify(apiCallPing)), 60000);
-
-    const tempArray = [];
     ws.onmessage = function (event) {
       try {
-        if (!event.data === '{"op":"pong"}') {
+        if (event.data !== '{"op":"pong"}') {
           const json = JSON.parse(event.data);
-          tempArray.push(json);
-          setBlocks(tempArray);
+          setBlocks(
+            blocksArray.map((block, index) => (
+              <BlockOut key={index}>
+                <p>{block.x.blockIndex}</p>
+              </BlockOut>
+            ))
+          );
+
+          blocksArray.unshift(json);
+          if (blocksArray.length > 5) blocksArray.pop();
+          setBlocksTimeout = setTimeout(() => {
+            setBlocks(
+              blocksArray.map((block, index) => (
+                <Block delay={(index + 1) / 2} key={index}>
+                  <p>{block.x.blockIndex}</p>
+                </Block>
+              ))
+            );
+          }, 800);
         }
       } catch (err) {
         console.log(err);
       }
     };
-    return () => ws.close();
+    return () => {
+      clearTimeout(setBlocksTimeout);
+      clearInterval(setPingInterval);
+      ws.close();
+    };
   }, []);
 
   return (
     <div className="blocks-explorer">
       <Navbar />
-      <Blocks>
-        {blocks.map((block) => (
-          <Block key={block.blockIndex}>
-            <p>{block.height}</p>
-          </Block>
-        ))}
-      </Blocks>
+      <Blocks>{blocks}</Blocks>
     </div>
   );
 };
@@ -60,6 +80,27 @@ const Block = styled.div`
   color: black;
   text-align: center;
   background-color: pink;
+  animation: fade-in ${(props) => props.delay || "0"}s linear;
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const BlockOut = styled(Block)`
+  animation: fade-out 0.8s linear;
+  @keyframes fade-out {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
 `;
 
 export default BlocksExplorer;
